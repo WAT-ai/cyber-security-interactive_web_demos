32725ab2d5b8988c6feba7c771c6df67afef2a3d
Madhav Malhotra <madhav.malhotra003@gmail.com>
2023-12-31 13:26:53 -0500

    For the first prototype, just created a quick temporary Vanilla JS demo

--- web-demo/src/App.js
+++ web-demo/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
-import Navbar from './Navbar';
-import Header from './Header';
 
 import Introduction from './Introduction';
 import KMeans from './KMeans';
-import DBScan from './DBScan';
-import NegativeSelection from './NegativeSelection';
 import Conclusion from './Conclusion';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 
 
 function App() {
-
-  
   return (
 
     
@@ -23,35 +17,19 @@ function App() {
       <div className="App">      
         
         <div className="content">
-              <Introduction />
+          <Introduction />
         </div>   
        
-      <div className="model">
-        <KMeans />
-      </div>
+        <div className="model">
+          <KMeans />
+        </div>
 
-            
-        
-   
+        <div className="section">
+          <Conclusion />
+        </div>  
       
-
-      <div className="section">
-        <Conclusion />
-      </div>  
-      
-
     </div>
     </Router>
-    
-       /*  PUT THIS BETWEEN MODEL AND SECTION
-      <div className="model">
-        <DBScan />
-      </div>  
-
-      <div className="model">
-        <NegativeSelection />
-      </div>  
-      */
   );
 }
 
--- web-demo/src/Introduction.js
+++ web-demo/src/Introduction.js
@@ -14,21 +14,36 @@ const Introduction = () => {
 
             <div>
                 <h1>Objective</h1>
+                <p>
+                    Our team has been trying to detect hackers' network activity 
+                    on smart devices. This is difficult because smart devices 
+                    like Amazon Alexas or security cameras are usually powered 
+                    by small microcontrollers. Thus, it's impossible to run 
+                    intensive software on them. 
+                </p>
             </div>
 
             <div>
                 <h1>Dataset</h1>
-            </div>
-            
-            
+                <p>
+                    Even worse, we've been working with the Canadian Institute 
+                    of Cybersecurity's IoT 2023 dataset, which has over 14 GB of 
+                    data! Clearly, we can't store (yet alone train algorithms on) a
+                    ll this data on a small smart device. Thus, we have some 
+                    tricky design requirements in creating ways to detect hackers:
+                </p>
+                <ol>
+                    <li>
+                        We had to very carefully find the best statistics about hackers' network requests and use only that minimal information. Ex: We want to know if the hacker has a web request, but not down to nitty gritty details like whether HTTP or HTTPS protocols are used. 
+                    </li>
+                    <li>
+                        We needed an algorithm that we could train on a massive dataset in the cloud. The aim is to store the algorithm on a microcontroller without taking up much space. This is like the ‘knowledge' from the gigabytes of data is ‘distilled' into a much smaller format.
+                    </li>
+                </ol>
 
-
-            <div className="next-button">
-                <Link to='kmeans' smooth={true} duration={500} className="next-button-link">TO OUR MODEL </Link>
-            </div>
-            
-            
-            
+                <p>
+                    Given this context, there are a few unsupervised learning AI algorithms that can meet our needs. This is where the negative selection and K-means algorithms come in. </p>
+            </div>    
         </div>
         
     );
