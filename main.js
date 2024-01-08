cb75c0c76706272888325082d980afd630129def
Thenuja Liyanage <Liyanage.Thenuja@hotmail.com>
2024-01-07 20:10:05 -0800

    styling chnages, updated repo structure to deploy on github pages

--- main.js
+++ main.js
@@ -45,14 +45,14 @@ loadCSVData(false).then(([tensorData, arrayData]) => {
 
     kMeans(tensorData, numCentroids, numIters, eps).then((bestCentroids) => {
         createScatterPlot("init-regular-data-scatter", arrayData,
-            bestCentroids.arraySync(), "K-Means Clustering with PCA (Single Iteration)");
+            bestCentroids.arraySync(), "K-Means Clustering with PCA - Initial Centroids");
     });
 });
 
 
 function runkmeans() {
     const numCentroids = 10;
-    const numIters = 20;
+    const numIters = 1;
     const eps = 6.0;
 
     document.getElementById('btn-rawkmeans').style.display = 'none';
@@ -63,7 +63,7 @@ function runkmeans() {
     kMeans(load_tensorData, numCentroids, numIters, eps).then((bestCentroids) => {
         document.getElementById('txt-train').style.display = 'none'
         createScatterPlot("rawkmeans-regular-data-scatter", load_arrayData,
-            bestCentroids.arraySync(), "K-Means Clustering with PCA (10 Iterations)");
+            bestCentroids.arraySync(), "K-Means Clustering with PCA");
     });
 }
 
@@ -76,13 +76,13 @@ function runkmeans_normalized() {
 
     loadCSVData(true).then(([tensorData, arrayData]) => {
         const numCentroids = 10;
-        const numIters = 10;
+        const numIters = 1;
         const eps = 6.0;
 
         kMeans(tensorData, numCentroids, numIters, eps).then((bestCentroids) => {
             document.getElementById('txt-train_norm').style.display = 'none'
             createScatterPlot("normkmeans-regular-data-scatter", arrayData,
-                bestCentroids.arraySync(), "K-Means Clustering with PCA - Normalized (10 Iterations)");
+                bestCentroids.arraySync(), "K-Means Clustering with PCA - Normalized");
         });
     });
 }
--- web-demo/src/KMeans.js
+++ web-demo/src/KMeans.js
@@ -5,286 +5,147 @@ import './Model.css'
 import Papa from 'papaparse';
 import * as tfVis from '@tensorflow/tfjs-vis';
 
-
-const KMeans = () => {
-    const [isTraining, setIsTraining] = useState(false);
-    const [afterTraining, setAfterTraining] = useState(false);
-    const [trainingProgress, setTrainingProgress] = useState(0);
-    const [trainingTime, setTrainingTime] = useState(0);
-    const [memoryUsage, setMemoryUsage] = useState(0);
-    const [data, setData] = useState([1]);
-    const [showResults, setShowResults] = useState(false);
-    const [results, setResults] = useState(null);
+/* 
+@file               kmeans-naive.js
+@brief              K-means clustering with random centroid initialisation
+@author             Aarush Ghosh, 
+*/
 
 
-    const ResultsPopup = ({ onClose }) => {
-        const [currentPage, setCurrentPage] = useState(1);
-        const handleNextPage = () => {
-            if (currentPage < 4) {
-                setCurrentPage(currentPage + 1);
-            }
-        };
-
-        const handlePrevPage = () => {
-            if (currentPage > 1) {
-                setCurrentPage(currentPage - 1);
-            }
-        };
-        const renderPageContent = () => {
-            switch (currentPage) {
-                case 1:
-                    return <p>Results: {results ? results.join(", ") : "No results"}</p>;
-                case 2:
-                    return <p>Time To Train: {trainingTime} seconds</p>;
-                case 3:
-                    return <p>Memory Usage: {memoryUsage}</p>;
-                case 4:
-                    return <p>Page 4 content</p>;
-                
-                default:
-                    return null;
-            }
-        };
-
-        return (
-          <div className="results-popup">
-            {renderPageContent()}
-            
-    
-            <div className='result-toggle'>
-                <button onClick={handlePrevPage} className='result-toggle-button'>Previous</button>
-                <button onClick={handleNextPage} className='result-toggle-button'>Next</button>
-            </div>
-
-
-            <div className='close'>
-                <button onClick={onClose} >Close</button>
-            </div>
-            
-          </div>
-        );
-    };
-
-    const handleTrainClick = async () => {
-        setIsTraining(true);
-        const startTime = performance.now();
-        const startMemory = performance.memory;
-
-  
-          
-
-    
-        // TensorFlow.js
-        // IMPLEMENTING NAIVE KMEANS
-
-        // Translation Functions
-        function genRandLike(shape, data) {
-            return tf.randomNormal(shape, 0, 1, 'float32');
-        }
-        
-        // Function to compute pairwise affinity between data and centroids
-        function affinity(data, centroids) {
-            const distances = tf.sub(data, centroids);
-            return tf.norm(distances, 2, 2).square();
-        }
-        
-        // KMeans Function
-        async function kMeans(data, numCentroids, numIters, eps = 0.1, centr = null) {
-            let best = null;
-        
-            // Fresh start (reset all centroids) on each iteration
-            for (let iter = 0; iter < numIters; iter++) {
-                console.log(iter);
-        
-                // Init random, prior, and best centroids
-                if (centr === null) {
-                    centr = genRandLike([numCentroids, data.shape[1]], data);
-                }
-                let prior = null;
-        
-                // Always perform at least one iteration
-                while (true) {
-                    console.log('.');
-        
-                    // Find centroids which are close to and not close to data
-                    const distances = affinity(tf.expandDims(data, 1), centr);
-                    const groups = tf.argMin(distances, 1).dataSync();
-                    const hasData = Array.from(new Set(groups));
-                    //const hasNoData = tf.compat.setdiff1d(tf.range(centr.shape[0]), hasData).dataSync();
-                    const allIndices = Array.from({ length: centr.shape[0] }, (_, i) => i);  //size 5
-                    console.log('Allindecies: ', allIndices)
-                    const hasNoData = allIndices.filter((index) => !hasData.includes(index)); //size 4
-                    console.log('hasData: ', hasData)
-                    const flattenedIndices = hasNoData.map((index) => index * centr.shape[1]);
-        
-                    for (let index = 0; index < hasNoData.length; index++) {
-                        centr[index] = genRandLike(21, data)
-                    }
-                    const updates = genRandLike([hasNoData.length * centr.shape[1]], data);
-                    const rowsToUpdate = tf.tensor(flattenedIndices.map((index) => Array.from({ length: centr.shape[1] }, (_, i) => updates[index + i])));
-                    /* target
-                        groups = np.argmin(affinity(np.expand_dims(data, 1), centr), axis=1)
-                        has_data = np.unique(groups)
-                        has_no_data = np.setdiff1d(np.arange(centr.shape[0]), has_data, assume_unique=True)
-            
-                        # Randomly replace centroids not close to data
-                        prior = centr.copy()
-                        centr[has_no_data, :] = gen_rand_like((has_no_data.shape[0], data.shape[-1]), data)
-                    */
-                    
-                    // Randomly replace centroids not close to data
-                    prior = centr.clone();
-                    centr = tf.mul(centr, tf.transpose(tf.scalar(1).sub(rowsToUpdate))).add(rowsToUpdate);
-                    
-                    //centr[has_no_data, :] = gen_rand_like((has_no_data.shape[0], data.shape[-1]), data)
-                    // const randomData = tf.randomNormal([numRows, numCols]);
-                    console.log("Before where:", centr.shape);
-                     
-        
-                        /*  
-                        const numRows = 1;
-                        const numCols = 5;  // Number of columns in the tensor
-
-                        // Generate random data for a tensor's row
-                        const randomData = tf.randomNormal([numRows, numCols]);
-
-                        // Print the generated tensor
-                        randomData.print();
-                        */
-        
-                    
-        
-                    // The problematic line with 'where'
-                    console.log("Center: ", centr);
-        
-                    // Update other centroids to be the average of their datapoints
-                    for (let i = 0; i < hasData.length; i++) {
-                        // console.log("Before where: 2", centr.shape);
-                        // console.log("Groups shape: ", groups);
-                        // console.log("Groups shape: ", hasData[i]);
-        
-        
-                        console.log("centr shape:", centr.shape);
-                        console.log("rowsToUpdate shape:", rowsToUpdate.shape);
-        
-                        const points = data.gather(tf.where(tf.equal(groups, hasData[i]))).squeeze(0);
-                        centr = centr.scatter([hasData[i]], tf.mean(points, 0));
-                    }
-        
-                    // If centroids not changing, stop
-                    if (tf.norm(affinity(prior, centr), 2, 1).less(eps).all().dataSync()[0]) {
-                        break;
-                    }
-                }
-        
-                // Find the total distance for all datapoints to their centroids as a correlate of accuracy
-                const dist = tf.sum(tf.min(affinity(tf.expandDims(data, 1), centr), 1)).dataSync()[0];
-        
-                if (best === null || best[0] > dist) {
-                    best = [dist, centr.clone()];
-                }
-            }
-        
-            // Return best accuracy centroids
-            return best[-1];
-        }
-        
-        // Function to load and preprocess the CSV data
-        async function loadCSVData() {
-            // Replace "your/local/path/your_data.csv" with the path to your local CSV file
-            const csvUrl = "0.001percent_2classes.csv";
-        
-            // Fetch the CSV file
-            const response = await fetch(csvUrl);
-            const csvText = await response.text();
-        
-            // Parse CSV text into an array of objects
-            const csvData = Papa.parse(csvText, { header: true, dynamicTyping: true }).data;
-        
-            // Extract features and convert to Tensor
-            const features = ["duration", "srate", /* ... */];
-            const xData = csvData.map((row) => features.map((feature) => row[feature]));
-            const benignData = tf.tensor2d(xData);
-        
-            return benignData;
-        }
-
-        loadCSVData().then((benignData) => {
-            // Now you can use benignData in your K-Means or other computations
-            const numCentroids = 5;
-            const numIters = 1;
-            const eps = 1.0;
-            console.log(benignData)
-            kMeans(benignData, numCentroids, numIters, eps).then((bestCentroids) => {
-                console.log(bestCentroids.shape);
-            });
-        });
-        
-
-
-        // END OF OF KMEANS
-
-        const endTime = performance.now();
-        const endMemory = performance.memory;
-        setTrainingTime(((endTime-startTime)/1000).toFixed(2)); 
-        setMemoryUsage(endMemory-startMemory);       
-
-        
-        setResults();
-
-        setIsTraining(false);
-        setAfterTraining(true);
-        setData([2,3]);
-    };
-
-    const handleViewResultsClick = () => {
-        setShowResults(true);
-    };
-    
-    const handleCloseResults = () => {
-        setShowResults(false);
-    };
-    
-
-    return (  
-        <div className='kmeans' style={{ height: '100vh', backgroundColor: '#333' }}>
-            
-            <div style={{  display: "flex", flexDirection: "row", alignItems: "center"  }} className="title">
-                
-                <h1 className="title-text">KMeans</h1>
-                <button className="train-button" onClick={afterTraining? handleViewResultsClick : handleTrainClick} > 
-                    {afterTraining? "View Results!" : isTraining ? "Training..." : "Train Model"}
-                </button>
-                
-
-                {(isTraining || afterTraining) && (
-                    <div style={{ flex: 10, marginLeft: "20px"}}>
-                        <p></p>
-                        <div
-                        style={{
-                        width: `${trainingProgress/4}%`,
-                        height: "20px",
-                        backgroundColor: "lime",
-                        }}
-                        ><p style={{ textAlign:'left', marginLeft:"20px"}}>{Math.floor(trainingProgress)}%</p></div>
-                    </div>
-                )}
-
-            </div>
-            <p>{data}</p>
-
-            <div className="next-button">
-                <Link to="conclusion" smooth={true} duration={500} className='next-button-link'>TO CONCLUSION </Link>
-            </div>
-
-            
-            {showResults && <ResultsPopup onClose={handleCloseResults} />}
-
-            <h1>Explain how model works</h1>
-            
-       </div>
-        
-    );
+//function to generate random values like NumPy's randn
+function genRandLike(shape) {
+    return tf.randomNormal(shape, 0, 1, 'float32');
 }
- 
-export default KMeans;
+
+// Compute Euclidean distance between data and centroids
+function affinity(data, centroids) {
+    const distances = tf.sub(data, centroids).square();
+    return tf.sum(distances, distances.shape.length - 1).sqrt();
+}
+
+// Main Function to perform K-Means clustering in TensorFlow.js
+async function kMeans(data, numCentroids, numIters, eps = 0.1, centr = null) {
+    let best = null;
+
+    // Fresh start (reset all centroids) on each iteration
+    for (let iter = 0; iter < numIters; iter++) {
+        console.log("Iteration number: ", iter+1);
+
+        // Init random, prior, and best centroids
+        if (centr === null) {
+            centr = genRandLike([numCentroids, data.shape[1]]);    // has shape [num_centr x 2]
+        }
+        const [nCentr, nDims] = centr.shape;
+        const nEx = data.shape[0];
+
+        // Always perform at least one update on centroid values
+        let numUpdates = 0;
+        while (true) {
+            const prior = centr.clone();
+
+            // Find centroids which are close to and not close to data
+            const distances = affinity(tf.expandDims(data, 1), centr);                // num_examples x num_centroids
+            const groupsOG = distances.argMin(1);
+            const groups = groupsOG.dataSync();                                       // num_examples
+
+            // save best centroids
+            if (best === null || best[0] > distances.sum()) {
+                best = [distances.sum(), centr.clone()];
+            }
+            
+            // figure out which centroids have data grouped under them
+            const allIndices = Array.from({ length: nCentr }, (_, i) => i);   // [0, 1, 2, ... num_centroids]
+            const hasData = Array.from(new Set(groups));                              // from above, filter to centroids with datapoints
+            const hasNoData = allIndices.filter((index) => !hasData.includes(index)); // from above filter to centroids w/o datapoints
+
+            // Replace the current centroid values with random ones
+            for (let i of hasNoData) {
+                const randTensor = genRandLike([1, nDims]);
+                centr = tf.concat([
+                    centr.slice([0, 0], [i, nDims]),       // data before centr to replace
+                    randTensor,                            // new data for centr to replace
+                    centr.slice([i + 1, 0], [-1, nDims])   // data after centr to replace
+                ]);
+            }
+
+            // Update other centroids to be the average of their datapoints
+            // this creates [0, 1, 2, 3, ..., num_examples]
+            const indices = tf.linspace(0, nEx - 2, nEx);
+
+            // this goes through the ith centroid
+            for(let i of hasData){
+                // finds which datapoints are grouped into the ith centroid
+                const comparison = tf.fill(groupsOG.shape, i);
+                const cond = tf.equal(groupsOG, comparison); 
+
+                // gets indices, and then raw values, of those datapoints 
+                let closest_to_centr = await tf.booleanMaskAsync(indices, cond);
+                closest_to_centr = tf.cast(closest_to_centr, 'int32');
+                const selected_datapoints = data.gather(closest_to_centr);
+
+                // setting the value of the ith centroid to be the mean of datapoints grouped under it
+                let newCentr = tf.mean(selected_datapoints, axis=0);
+                newCentr = tf.expandDims(newCentr, axis=0);
+
+                centr = tf.concat([
+                    centr.slice([0, 0], [i, nDims]),       // data before centr to replace
+                    newCentr,                              // new data for centr to replace
+                    centr.slice([i + 1, 0], [-1, nDims])   // data after centr to replace
+                ]);                
+            }   
+
+            // If centroids not changing, stop
+            const change = affinity(prior, centr).sum();
+            numUpdates += 1;
+            if (change < eps || numUpdates > 5) break;
+            else {
+                console.log("Change in centroid values: ");
+                change.print();
+            }
+        }
+    }
+
+    // Return best accuracy centroids
+    return best[1];
+}
+
+// Function to load and preprocess the CSV data
+async function loadCSVData(normalized = false) {
+    const csvUrl = "0.001percent_2classes.csv";
+
+    // Fetch the CSV file
+    const response = await fetch(csvUrl);
+    const csvText = await response.text();
+
+    // Parse CSV text into an array of objects
+    const csvData = Papa.parse(csvText, { header: true, dynamicTyping: true }).data;
+
+    // Extract features and convert to Tensor
+    const features = ["duration", "srate", /* ... */];
+    const xData = csvData.map((row) => features.map((feature) => row[feature]));
+    
+    // Only keep max 1K rows (algorithm already slow)
+    let benignData = tf.tensor2d(xData).slice([0,0], [1000,2]);
+
+    // normalization (optional)
+    if (normalized) {
+        const {mean, variance} = tf.moments(benignData, 0);
+        benignData = tf.sub(benignData, mean).div(tf.sqrt(variance));
+    }
+
+    return benignData;
+}
+
+loadCSVData(true).then((benignData) => {
+    // Now you can use benignData in your K-Means or other computations
+    const numCentroids = 10;
+    const numIters = 1;
+    const eps = 6.0;
+
+    kMeans(benignData, numCentroids, numIters, eps).then((bestCentroids) => {
+        bestCentroids.print();
+    });
+});
+
+
+
